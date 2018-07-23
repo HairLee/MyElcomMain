fae25bbf7b62e78eddd81c2d29dfa60de46a41f3
HairLee <hailpt.hust@gmail.com>
2018-07-23 18:06:45 +0700

    cm

--- app/src/main/java/com/elcom/hailpt/view/ui/fragment/news/NewsFragment.java
+++ app/src/main/java/com/elcom/hailpt/view/ui/fragment/news/NewsFragment.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +17,12 @@ import android.view.ViewGroup;
 import com.elcom.hailpt.R;
 import com.elcom.hailpt.core.base.BaseFragment;
 import com.elcom.hailpt.model.api.RestData;
+import com.elcom.hailpt.model.api.response.News;
 import com.elcom.hailpt.model.api.response.NewsRes;
+import com.elcom.hailpt.view.adapter.ElcomNewsAdapter;
 import com.elcom.hailpt.viewmodel.NewsViewModel;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +32,7 @@ public class NewsFragment extends BaseFragment<NewsViewModel> {
     private RecyclerView recyclerViewTop;
     private RecyclerView recyclerViewBottom;
     private NewsViewModel newsViewModel;
+    private ElcomNewsAdapter elcomNewsAdapter;
     public NewsFragment() {
         // Required empty public constructor
     }
@@ -62,15 +69,16 @@ public class NewsFragment extends BaseFragment<NewsViewModel> {
             public void onChanged(@Nullable RestData<NewsRes> newsResRestData) {
 
                 Log.e("hailpt"," "+newsResRestData.message);
-
+                setupRecyclerViewTop(newsResRestData.data.getHot_articles());
             }
         });
-
     }
 
-    private void setupRecyclerViewTop(){
-
-
+    private void setupRecyclerViewTop(List<News> news){
+        elcomNewsAdapter = new ElcomNewsAdapter();
+        elcomNewsAdapter.setData(news);
+        recyclerViewTop.setLayoutManager(new StaggeredGridLayoutManager(1, OrientationHelper.HORIZONTAL));
+        recyclerViewTop.setAdapter(elcomNewsAdapter);
     }
 
     private void setupRecyclerViewBottom(){
