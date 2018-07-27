6a76d0993a7602f0807413361694c6fe8d756a17
HairLee <hailpt.hust@gmail.com>
2018-07-27 14:26:07 +0700

    cm

---
+++ app/src/main/java/com/elcom/myelcom/services/OneSignalNotificationReceivedHandler.java
@@ -0,0 +1,25 @@
+package com.elcom.myelcom.services;
+
+import android.util.Log;
+
+import com.onesignal.OSNotification;
+import com.onesignal.OneSignal;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Hailpt on 7/27/2018.
+ */
+public class OneSignalNotificationReceivedHandler implements OneSignal.NotificationReceivedHandler {
+    @Override
+    public void notificationReceived(OSNotification notification) {
+        JSONObject data = notification.payload.additionalData;
+        String customKey;
+
+        if (data != null) {
+            customKey = data.optString("customkey", null);
+            if (customKey != null)
+                Log.i("OneSignalExample", "customkey set with value: " + customKey);
+        }
+    }
+}
